a64b51731f88bfb6d8c21f76b071e48df05b39d9
Gobinda9732 <gobindajana9732@gmail.com>
2026-01-05 14:47:56 +0530

    change bse Url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3001/api",
+    baseURL: "https://job-portal-backend-vercel.vercel.app/api"
 });
 
 api.interceptors.request.use((config) => {
